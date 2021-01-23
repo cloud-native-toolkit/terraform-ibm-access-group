d71769a8ca26944d8fe842d293f12a87f8ef0402
Sean Sundberg <seansund@us.ibm.com>
2021-01-22 22:16:06 -0600

    Adds logic to create access groups and apply simple rules (#1)

---
+++ test/stages/stage2-access-group.tf
@@ -0,0 +1,7 @@
+module "access_groups" {
+  source = "./module"
+
+  region               = var.region
+  resourceGroupNames   = var.new_resource_group
+  createResourceGroups = true
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,15 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "resourceGroupNames" {
+  type        = string
+  description = "Comma-separated list of resource group names that should be created"
+}
 
+variable "region" {
+  type        = string
+  description = "The region where the container registry should be set up"
+}
+
+variable "createResourceGroups" {
+  type        = bool
+  description = "Flag indicating that the resource groups should be created"
+  default     = false
+}
