d84cc38cb2e5c3cb54013922dd19f263271a319c
Sean Sundberg <seansund@us.ibm.com>
2021-02-11 14:36:17 -0600

    Fixes access group validation logic (#4)

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
 
-set -x
+set -e
 
 ibmcloud login --apikey "${IBMCLOUD_API_KEY}" -r us-east
 
-source ./terraform.tfvars
+RESOURCE_GROUP=$(cat ./terraform.tfvars | grep "new_resource_group" | sed -E "s/.*=//g" | sed 's/"//g')
 
-ACCESS_GROUP_BASE=$(echo "${new_resource_group}" | sed "s/-/_/g" | tr '[:lower:]' '[:upper:]')
-ADMIN_ACCESS_GROUP="${ACCESS_GROUP_BASE}-ADMIN"
-EDIT_ACCESS_GROUP="${ACCESS_GROUP_BASE}-EDIT"
-VIEW_ACCESS_GROUP="${ACCESS_GROUP_BASE}-VIEW"
+echo "Resource group: ${RESOURCE_GROUP}"
 
-declare -a GROUPS=(${ADMIN_ACCESS_GROUP} ${EDIT_ACCESS_GROUP} ${VIEW_ACCESS_GROUP})
+ACCESS_GROUP_BASE=$(echo "${RESOURCE_GROUP}" | sed "s/-/_/g" | tr '[:lower:]' '[:upper:]')
 
-for group in "${GROUPS[@]}"; do
+echo "Access group base: ${ACCESS_GROUP_BASE}"
+
+ADMIN_ACCESS_GROUP="${ACCESS_GROUP_BASE}_ADMIN"
+EDIT_ACCESS_GROUP="${ACCESS_GROUP_BASE}_EDIT"
+VIEW_ACCESS_GROUP="${ACCESS_GROUP_BASE}_VIEW"
+
+ACCESS_GROUPS="${ADMIN_ACCESS_GROUP} ${EDIT_ACCESS_GROUP} ${VIEW_ACCESS_GROUP}"
+
+echo "Groups: ${ACCESS_GROUPS}"
+
+for group in ${ACCESS_GROUPS}; do
+  echo "Checking for group: ${group}"
+
   if ! ibmcloud iam access-group "${group}"; then
     echo "Access group not found: ${group}"
     exit 1
