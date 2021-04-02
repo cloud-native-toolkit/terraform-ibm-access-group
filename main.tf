d9e67ecaa925e9bd0107e718f662870aa6670326
Sean Sundberg <seansund@us.ibm.com>
2021-04-01 22:24:57 -0500

    Prints group names
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -1,28 +1,42 @@
 
 /*** Create Access Groups for Admins and Users ***/
+locals {
+  admin_name  = "${replace(upper(var.resource_group_name), "-", "_")}_ADMIN"
+  editor_name = "${replace(upper(var.resource_group_name), "-", "_")}_EDIT"
+  viewer_name = "${replace(upper(var.resource_group_name), "-", "_")}_VIEW"
+}
 
-resource "null_resource" "print_resource_group" {
+resource "null_resource" "print_names" {
   provisioner "local-exec" {
     command = "echo 'Resource group: ${var.resource_group_name}'"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Admin access group: ${local.admin_name}'"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Editor access group: ${local.editor_name}'"
+  }
+  provisioner "local-exec" {
+    command = "echo 'Viewer access group: ${local.viewer_name}'"
   }
 }
 
 data ibm_resource_group resource_group {
-  depends_on = [null_resource.print_resource_group]
+  depends_on = [null_resource.print_names]
 
   name = var.resource_group_name
 }
 
 resource ibm_iam_access_group admins {
-  name  = "${replace(upper(var.resource_group_name), "-", "_")}_ADMIN"
+  name = local.admin_name
 }
 
 resource ibm_iam_access_group editors {
-  name  = "${replace(upper(var.resource_group_name), "-", "_")}_EDIT"
+  name = local.editor_name
 }
 
 resource ibm_iam_access_group viewers {
-  name  = "${replace(upper(var.resource_group_name), "-", "_")}_VIEW"
+  name = local.viewer_name
 }
 
 /*** Import resource groups for the Admins Access Groups ***/
