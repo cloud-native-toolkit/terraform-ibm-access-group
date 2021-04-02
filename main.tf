2a11643eeaa916d149a07f22e76de29c63d40e5f
Sean Sundberg <seansund@us.ibm.com>
2021-04-01 22:12:16 -0500

    Updates structure
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- main.tf
+++ main.tf
@@ -12,71 +12,64 @@ resource "null_resource" "print_resource_group" {
 }
 
 data ibm_resource_group resource_group {
-  count = local.resourceGroupCount
   depends_on = [null_resource.print_resource_group]
 
-  name = local.resourceGroupNames[count.index]
+  name = var.resource_group_name
 }
 
-resource "ibm_iam_access_group" "admins" {
-  count = local.resourceGroupCount
-
-  name  = "${replace(upper(local.resourceGroupNames[count.index]), "-", "_")}_ADMIN"
+resource ibm_iam_access_group admins {
+  name  = "${replace(upper(var.resource_group_name), "-", "_")}_ADMIN"
 }
 
 resource ibm_iam_access_group editors {
-  count = local.resourceGroupCount
-
-  name  = "${replace(upper(local.resourceGroupNames[count.index]), "-", "_")}_EDIT"
+  name  = "${replace(upper(var.resource_group_name), "-", "_")}_EDIT"
 }
 
 resource ibm_iam_access_group viewers {
-  count = local.resourceGroupCount
-
-  name  = "${replace(upper(local.resourceGroupNames[count.index]), "-", "_")}_VIEW"
+  name  = "${replace(upper(var.resource_group_name), "-", "_")}_VIEW"
 }
 
 /*** Import resource groups for the Admins Access Groups ***/
 
 /*** Admins Access Groups Policies ***/
 
-resource "ibm_iam_access_group_policy" "admin_policy_1" {
-  count = local.resourceGroupCount
+resource ibm_iam_access_group_policy admin_policy_1 {
 
-  access_group_id = element(ibm_iam_access_group.admins.*.id, count.index)
+  access_group_id = element(ibm_iam_access_group.admins.id, count.index)
   roles           = ["Editor", "Manager"]
+
   resources {
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
   }
 }
 
-resource "ibm_iam_access_group_policy" "admin_policy_2" {
-  count = local.resourceGroupCount
+resource ibm_iam_access_group_policy admin_policy_2 {
 
   access_group_id = element(ibm_iam_access_group.admins.*.id, count.index)
   roles           = ["Viewer"]
+
   resources {
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
-    attributes        = { "resourceType" = "resource-group", "resource" = local.resourceGroupNames[count.index] }
+    resource_group_id = data.ibm_resource_group.resource_group.id
+    attributes        = { "resourceType" = "resource-group", "resource" = var.resource_group_name }
   }
 }
 
-resource "ibm_iam_access_group_policy" "admin_policy_3" {
-  count = local.resourceGroupCount
+resource ibm_iam_access_group_policy admin_policy_3 {
 
-  access_group_id = element(ibm_iam_access_group.admins.*.id, count.index)
+  access_group_id = ibm_iam_access_group.admins.id
   roles           = ["Administrator", "Manager"]
+
   resources {
     service           = "containers-kubernetes"
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
   }
 }
 
-resource "ibm_iam_access_group_policy" "admin_policy_4" {
-  count = local.resourceGroupCount
+resource ibm_iam_access_group_policy admin_policy_4 {
 
-  access_group_id = element(ibm_iam_access_group.admins.*.id, count.index)
+  access_group_id = ibm_iam_access_group.admins.id
   roles           = ["Administrator", "Manager"]
+
   resources {
     service = "container-registry"
   }
@@ -85,45 +78,45 @@ resource "ibm_iam_access_group_policy" "admin_policy_4" {
 /*** Editor Access Groups Policies ***/
 
 resource ibm_iam_access_group_policy edit_policy_1 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.editors.*.id, count.index)
+  access_group_id = ibm_iam_access_group.editors.id
   roles           = ["Viewer", "Manager"]
+
   resources {
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
   }
 }
 
 resource ibm_iam_access_group_policy edit_policy_2 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.editors.*.id, count.index)
+  access_group_id = ibm_iam_access_group.editors.id
   roles           = ["Viewer"]
+
   resources {
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
-    attributes        = { "resourceType" = "resource-group", "resource" = local.resourceGroupNames[count.index] }
+    resource_group_id = data.ibm_resource_group.resource_group.id
+    attributes        = { "resourceType" = "resource-group", "resource" = var.resource_group_name }
   }
 }
 
 resource ibm_iam_access_group_policy edit_policy_3 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.editors.*.id, count.index)
+  access_group_id = ibm_iam_access_group.editors.id
   roles           = ["Editor", "Writer"]
+
   resources {
     service           = "containers-kubernetes"
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
   }
 }
 
 resource ibm_iam_access_group_policy edit_policy_4 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.editors.*.id, count.index)
+  access_group_id = ibm_iam_access_group.editors.id
   roles           = ["Reader", "Writer"]
+
   resources {
     resource_type     = "namespace"
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
     service           = "container-registry"
   }
 }
@@ -132,46 +125,45 @@ resource ibm_iam_access_group_policy edit_policy_4 {
 /*** Viewer Access Groups Policies ***/
 
 resource ibm_iam_access_group_policy view_policy_1 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.viewers.*.id, count.index)
+  access_group_id = ibm_iam_access_group.viewers.id
   roles           = ["Viewer", "Reader"]
+
   resources {
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
   }
 }
 
 resource ibm_iam_access_group_policy view_policy_2 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.viewers.*.id, count.index)
+  access_group_id = ibm_iam_access_group.viewers.id
   roles           = ["Viewer"]
+
   resources {
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
-    attributes        = { "resourceType" = "resource-group", "resource" = local.resourceGroupNames[count.index] }
+    resource_group_id = data.ibm_resource_group.resource_group.id
+    attributes        = { "resourceType" = "resource-group", "resource" = var.resource_group_name }
   }
 }
 
 resource ibm_iam_access_group_policy view_policy_3 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.viewers.*.id, count.index)
+  access_group_id = ibm_iam_access_group.viewers.id
   roles           = ["Viewer", "Reader"]
+
   resources {
     service           = "containers-kubernetes"
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
   }
 }
 
 resource ibm_iam_access_group_policy view_policy_4 {
-  count = local.resourceGroupCount
 
-  access_group_id = element(ibm_iam_access_group.viewers.*.id, count.index)
+  access_group_id = ibm_iam_access_group.viewers.id
   roles           = ["Viewer", "Reader"]
+
   resources {
     resource_type     = "namespace"
-    resource_group_id = element(data.ibm_resource_group.resource_group.*.id, count.index)
+    resource_group_id = data.ibm_resource_group.resource_group.id
     service           = "container-registry"
   }
 }
-
--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "resource_group_name" {
   description = "List of resource group names"
-  value       = local.resourceGroupNames[0]
+  value       = var.resource_group_name
   depends_on  = [data.ibm_resource_group.resource_group]
 }
 
 output "admin_group_name" {
   description = "List of admin access group names"
-  value       = ibm_iam_access_group.admins[0].name
+  value       = ibm_iam_access_group.admins.name
   depends_on  = [
     ibm_iam_access_group_policy.admin_policy_1,
     ibm_iam_access_group_policy.admin_policy_2,
@@ -17,7 +17,7 @@ output "admin_group_name" {
 
 output "edit_group_name" {
   description = "List of editor access group names"
-  value       = ibm_iam_access_group.editors[0].name
+  value       = ibm_iam_access_group.editors.name
   depends_on  = [
     ibm_iam_access_group_policy.edit_policy_1,
     ibm_iam_access_group_policy.edit_policy_2,
@@ -28,7 +28,7 @@ output "edit_group_name" {
 
 output "view_group_name" {
   description = "List of viewer access group names"
-  value       = ibm_iam_access_group.viewers[0].name
+  value       = ibm_iam_access_group.viewers.name
   depends_on  = [
     ibm_iam_access_group_policy.view_policy_1,
     ibm_iam_access_group_policy.view_policy_2,
