7cb9e93da66035a1277f82de19f94e204de7cb96
Sean Sundberg <seansund@us.ibm.com>
2021-04-01 22:28:04 -0500

    Use resource_group_name variable
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-resource-group.tf
+++ test/stages/stage1-resource-group.tf
@@ -1,5 +1,5 @@
 module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git"
 
-  resource_group_name = "test-resource-group"
+  resource_group_name = var.resource_group_name
 }
