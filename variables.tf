cfd648e5d92d715cb71dd265f151dbe8b2d33452
Sean Sundberg <seansund@us.ibm.com>
2021-06-21 22:46:46 -0500

    Adds ibmcloud_api_key as input and config for provider (#12)
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- variables.tf
+++ variables.tf
@@ -1,3 +1,8 @@
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The api key for IBM Cloud access"
+}
+
 variable "resource_group_name" {
   type        = string
   description = "The name of the resource group for which the access groups will be created"
