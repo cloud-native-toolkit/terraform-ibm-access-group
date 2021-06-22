73defccd0a65a67718fc057820b8f14b8376bcf3
Sean Sundberg <seansund@us.ibm.com>
2021-06-21 22:32:56 -0500

    Provision flag from resource_group provision
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-access-group.tf
+++ test/stages/stage2-access-group.tf
@@ -2,6 +2,6 @@ module "access_groups" {
   source = "./module"
 
   resource_group_name = module.resource_group.name
-  provision           = true
+  provision           = module.resource_group.provision
   ibmcloud_api_key    = var.ibmcloud_api_key
 }
