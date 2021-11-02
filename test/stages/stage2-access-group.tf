42825a4a5016862f831b63493d650648d1ee366e
Sean Sundberg <seansund@us.ibm.com>
2021-11-01 22:57:54 -0500

    Updates provider handling (#14)
    - Removes provider config from module
    - Removes ibmcloud_api_key variable
    - Updates tests with provider config changes
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-access-group.tf
+++ test/stages/stage2-access-group.tf
@@ -3,5 +3,4 @@ module "access_groups" {
 
   resource_group_name = module.resource_group.name
   provision           = module.resource_group.provision
-  ibmcloud_api_key    = var.ibmcloud_api_key
 }
