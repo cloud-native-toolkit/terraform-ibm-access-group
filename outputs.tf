56998cc90ccb845fb8ae002d1ceffc7e1df6ac20
Sean Sundberg <seansund@us.ibm.com>
2022-04-06 22:58:35 -0400

    Updates outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "resource_group_name" {
 
 output "admin_group_name" {
   description = "List of admin access group names"
-  value       = local.admin_name
+  value       = data.ibm_iam_access_group.admins.access_group_name
   depends_on  = [
     ibm_iam_access_group_policy.admin_policy_1,
     ibm_iam_access_group_policy.admin_policy_2,
@@ -17,7 +17,7 @@ output "admin_group_name" {
 
 output "edit_group_name" {
   description = "List of editor access group names"
-  value       = local.editor_name
+  value       = data.ibm_iam_access_group.editors.access_group_name
   depends_on  = [
     ibm_iam_access_group_policy.edit_policy_1,
     ibm_iam_access_group_policy.edit_policy_2,
@@ -28,7 +28,7 @@ output "edit_group_name" {
 
 output "view_group_name" {
   description = "List of viewer access group names"
-  value       = local.viewer_name
+  value       = data.ibm_iam_access_group.viewers.access_group_name
   depends_on  = [
     ibm_iam_access_group_policy.view_policy_1,
     ibm_iam_access_group_policy.view_policy_2,
