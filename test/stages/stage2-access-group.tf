4ebe2b3eb4d777f62cc90d9b62d3da8badfa1217
Sean Sundberg <seansund@us.ibm.com>
2022-04-06 22:34:59 -0400

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-access-group.tf
+++ test/stages/stage2-access-group.tf
@@ -2,5 +2,5 @@ module "access_groups" {
   source = "./module"
 
   resource_group_name = module.resource_group.name
-  provision           = module.resource_group.provision
+  ibmcloud_api_key    = var.ibmcloud_api_key
 }
