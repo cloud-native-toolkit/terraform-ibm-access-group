9e2eca4bf0681e3350385925fe995e7c7a50b4db
Andrew Trice <amtrice@us.ibm.com>
2022-04-12 00:00:49 -0400

    updated outputs
    Signed-off-by: Andrew Trice <amtrice@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -4,35 +4,35 @@ output "resource_group_name" {
   depends_on  = [data.ibm_resource_group.resource_group]
 }
 
-#output "admin_group_name" {
-#  description = "List of admin access group names"
-#  value       = data.ibm_iam_access_group.admins.access_group_name
-#  depends_on  = [
-#    ibm_iam_access_group_policy.admin_policy_1,
-#    ibm_iam_access_group_policy.admin_policy_2,
-#    ibm_iam_access_group_policy.admin_policy_3,
-#    ibm_iam_access_group_policy.admin_policy_4
-#  ]
-#}
-#
-#output "edit_group_name" {
-#  description = "List of editor access group names"
-#  value       = data.ibm_iam_access_group.editors.access_group_name
-#  depends_on  = [
-#    ibm_iam_access_group_policy.edit_policy_1,
-#    ibm_iam_access_group_policy.edit_policy_2,
-#    ibm_iam_access_group_policy.edit_policy_3,
-#    ibm_iam_access_group_policy.edit_policy_4
-#  ]
-#}
-#
-#output "view_group_name" {
-#  description = "List of viewer access group names"
-#  value       = data.ibm_iam_access_group.viewers.access_group_name
-#  depends_on  = [
-#    ibm_iam_access_group_policy.view_policy_1,
-#    ibm_iam_access_group_policy.view_policy_2,
-#    ibm_iam_access_group_policy.view_policy_3,
-#    ibm_iam_access_group_policy.view_policy_4
-#  ]
-#}
+output "admin_group_name" {
+  description = "List of admin access group names"
+  value       = data.ibm_iam_access_group.admins.access_group_name
+  depends_on  = [
+    null_resource.admin_policy_1,
+    null_resource.admin_policy_2,
+    null_resource.admin_policy_3,
+    null_resource.admin_policy_4
+  ]
+}
+
+output "edit_group_name" {
+  description = "List of editor access group names"
+  value       = data.ibm_iam_access_group.editors.access_group_name
+  depends_on  = [
+    null_resource.edit_policy_1,
+    null_resource.edit_policy_2,
+    null_resource.edit_policy_3,
+    null_resource.edit_policy_4
+  ]
+}
+
+output "view_group_name" {
+  description = "List of viewer access group names"
+  value       = data.ibm_iam_access_group.viewers.access_group_name
+  depends_on  = [
+    null_resource.view_policy_1,
+    null_resource.view_policy_2,
+    null_resource.view_policy_3,
+    null_resource.view_policy_4
+  ]
+}
