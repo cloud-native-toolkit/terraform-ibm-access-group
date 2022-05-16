2ce0b227a5b0c977ed6ae458877e4cc340e28c9b
Sean Sundberg <seansund@us.ibm.com>
2022-05-16 11:44:39 -0500

    Cleans up destroy logic for policies
    - Test intermediate values and exit without error if value missing
    - Tests for null description before regex test to avoid error
    
    closes #29
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/delete-access-group-policy.sh
+++ scripts/delete-access-group-policy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
-
-
-export PATH="$BIN_DIR:$PATH"
+if [[ -n "${BIN_DIR}" ]]; then
+  export PATH="$BIN_DIR:$PATH"
+fi
 
 if ! command -v jq 1> /dev/null 2> /dev/null; then
   echo "JQ command not found" >&2
@@ -15,6 +14,11 @@ if [[ -z "${IBMCLOUD_API_KEY}" ]]; then
   exit 1
 fi
 
+if [[ -z "${ACCESS_GROUP_ID}" ]]; then
+  echo "ACCESS_GROUP_ID must be provided as an environment variable" >&2
+  exit 1
+fi
+
 IAM_TOKEN=$(curl -s -X POST "https://iam.cloud.ibm.com/identity/token" \
   -H "Content-Type: application/x-www-form-urlencoded" \
   -d "grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=${IBMCLOUD_API_KEY}" | jq -r '.access_token')
@@ -23,27 +27,30 @@ ACCOUNT_ID=$(curl -s -X GET 'https://iam.cloud.ibm.com/v1/apikeys/details' \
   -H "Authorization: Bearer $IAM_TOKEN" -H "IAM-Apikey: ${IBMCLOUD_API_KEY}" \
   -H 'Content-Type: application/json' | jq -r '.account_id')
 
-#find access policies for the access group
+if [[ -z "${ACCOUNT_ID}" ]]; then
+  echo "ACCOUNT_ID not found" >&2
+  exit 0
+fi
 
-POLICIES=$(curl -s -X GET "https://iam.cloud.ibm.com/v1/policies?account_id=$ACCOUNT_ID&access_group_id=$ACCESS_GROUP_ID" \
+POLICIES=$(curl -s -X GET "https://iam.cloud.ibm.com/v1/policies?account_id=${ACCOUNT_ID}&access_group_id=${ACCESS_GROUP_ID}" \
   -H "Authorization: Bearer $IAM_TOKEN" \
   -H "Content-Type: application/json")
 
-
-#echo "DESCRIPTION: $DESCRIPTION"
-POLICY=$(echo $POLICIES | jq ".policies[] | select(.description==\"$DESCRIPTION\")" -r)
-POLICY_ID=$(echo $POLICY | jq '.id' -r )
-
-
-if [ -z "$POLICY" ]; then
-  echo "Policy not found."
-  exit
+if [[ -z "${POLICIES}" ]]; then
+  echo "Error retrieving policies: account_id=${ACCOUNT_ID}, access_group=${ACCESS_GROUP_ID}" >&2
+  exit 0
 fi
 
-#delete the policy
-echo "Deleting policy: $POLICY_ID..."
+POLICY_ID=$(echo "${POLICIES}" | jq --arg DESCRIPTION "${DESCRIPTION}" '.policies[] | select(.description != null) | select(.description == $DESCRIPTION) | .id // empty' -r)
+
+if [[ -z "${POLICY_ID}" ]]; then
+  echo "Policy with description not found: ${DESCRIPTION}" >&2
+  exit 0
+fi
+
+echo "Deleting policy: ${POLICY_ID}..."
 curl -s -X DELETE "https://iam.cloud.ibm.com/v1/policies/$POLICY_ID" \
   -H "Authorization: Bearer $IAM_TOKEN" \
   -H "Content-Type: application/json"
 
-echo "Deleted"
+echo "Policy deleted: ${POLICY_ID}"
